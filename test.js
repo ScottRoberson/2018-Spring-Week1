f3855e93137265545ade3b10f13848ffca7dc18f
Scott Roberson <srkazan23@gmail.com>
2018-03-03 12:25:30 -0600

    Moake our first javascript program.

---
+++ test.js
@@ -0,0 +1,19 @@
+var name = "Scott";
+var born =1978;
+var year= 2018;
+
+var age = year - born;
+
+console.log("My name is " + name);
+console.log("I am " + age + "years old");
+
+if( age > 40 ){
+	console.log("Damn...you are OLD!!!");
+} else {
+	console.log("Dang...I was hoping to be younger than you");
+	if (age==40) {
+		console.log(" Soon you will be old as well.");
+	} else {
+		console.log("I hate you!");
+	}
+}								
